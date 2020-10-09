8b3383d39d00febb487854961672e306e393bc3c
Shilpa Chegudi <shilpachegudi@gmail.com>
2020-10-09 03:19:05 +0000

    Alerts added for long tweets exceeding character count

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
  
   $("#tweet-text").keydown(function() {
     let tweet = $(this).val().length;
-    console.log(tweet);
     const limit = $('.counter');
     let maxVal = 140;
     limit.val(maxVal - tweet );
@@ -11,10 +10,22 @@ $(document).ready(function() {
     if (tweet > 140) {
 
       limit.css('color', 'red');
+      alert("Tweet is way too long, limit to 140 characters please!!")
+
     }
     else {
       limit.css('color', 'black');
     }
-
   });
+
+  $('#submit-button').click(function() {
+    $('.counter').val(140);
+    if ($('#tweet-text').val() === "") {
+      alert("Blanks tweets are not good!");
+
+    } else if ($('#tweet-text').val().length > 140) {
+      ralert("Tweet is too long, respect 140 character rule!!")
+
+    }
+  })
 });
