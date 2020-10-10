06b3af4d9ba8eeb4e1bcb3ed3533bfc90f193cc9
Shilpa Chegudi <shilpachegudi@gmail.com>
2020-10-10 23:38:31 +0000

    Code cleaning and removed unnecessary comments

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,12 +1,9 @@
-/*Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+/*Client-side JS logic goes here*/
  $(document).ready(function() {
 
 //This function evaluates the text input by user and re-encodes the text to avoid unsafe characters converting into safe 'encoded' form
    const escape =  function(str) {
-    let div = document.createElement('div');
+    let div     = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   }
@@ -23,24 +20,23 @@ const createTweetElement = function(data) {
   </div>
   <h2 class="tweet1">${escape(data.content.text)}</h2>
   <div class="tweet-footer">
-  <p class="days-ago"> ${data.created_at}}</p>
+  <p class="days-ago"> ${moment(data.created_at).toNow(true)} ago </p>
   <p class="small-icons"> <i class="fa fa-flag"></i> <i class="fa fa-retweet"></i><i class="fa fa-heart"></i></p>
-  </article>
+  </div>
   </div>`;
 
 return newTweet;
 };
 
-  //To toggle tweet input form
- $('.angled-arrow').click(function() {
-  $(".btn-and-input").slideToggle("slow");
-});
+ //To toggle tweet input form
+    $('.angled-arrow').click(function() {
+    $(".btn-and-input").slideToggle("slow");
+  });
  
  //Submit function for submitting a new tweet
   $( '#tweet-form').submit(function( event ) {
     event.preventDefault();
- //AJAX post request to send data to server
-  $.ajax({
+    $.ajax({
         url: '/tweets',
         method: "POST" ,
         data: $(this).serialize(),
@@ -49,7 +45,6 @@ return newTweet;
         console.log("Success")
         loadTweets()
       })
-    //}
 });
 
 // Loops through tweets and calls createTweetElement for each tweet.
@@ -63,8 +58,7 @@ const renderTweets = function(tweets) {
   }
 }
 
-  //ajax GET request
-  //loads current collection of tweets onto the page
+//Loads current collection of tweets onto the page
   const loadTweets = function() {
     $.ajax({
       url: '/tweets',
