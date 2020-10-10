1e35d5bc146ca7719b6f9990b53257772dc7b8cf
Shilpa Chegudi <shilpachegudi@gmail.com>
2020-10-10 18:21:59 +0000

    Added renderError function and also checked it's functionality

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
-  console.log("DOM Starts!")
- 
+
+  //Function to render error to user about tweet length!
+  const renderError = function(message) {
+    $('#error').remove();
+    const error = $(`<div id="error"> ${message} </div>`);
+    $('.new-tweet').prepend(error);
+    $('#error').slideDown("slow"); //animations;
+  };
   $("#tweet-text").keydown(function() {
     let tweet = $(this).val().length;
     const limit = $('.counter');
@@ -10,22 +16,22 @@ $(document).ready(function() {
     if (tweet > 140) {
 
       limit.css('color', 'red');
-      alert("Tweet is way too long, limit to 140 characters please!!")
-
+      renderError("Tweet way too long, please respect 140 character rule!!");
+      $('#submit_tweet').prop('disabled', true); //disable it
     }
     else {
       limit.css('color', 'black');
+      $('#error').remove();
+      $('#submit_tweet').prop('disabled', false); //enable it so it can submit
     }
   });
 
-  $('#submit-button').click(function() {
+  $('#submit_tweet').click(function() {
     $('.counter').val(140);
-    if ($('#tweet-text').val().length === "") {
-      alert("Blanks tweets are not good!");
-
+    if ($('#tweet-text').val() === "") {
+      renderError(" &#129488 Type something before you can post it!");
     } else if ($('#tweet-text').val().length > 140) {
-      ralert("Tweet is too long, respect 140 character rule!!")
-
+      renderError("Tweet way too long, please respect 140 character rule!!");
     }
-  })
+  });
 });
